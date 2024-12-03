b5c1694f98c4f3f6e38e8e0db9bc7e69d5d2e42d
Ali Alsowaidi <202104585@stu.uob.edu.bh>
2024-12-04 00:03:57 +0300

    login html

--- Login.php
+++ Login.php
@@ -18,3 +18,27 @@
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2 class="mt-5">Login</h2>
+        <form action="Login.php" method="post">
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</body>
+</html>
