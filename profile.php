73f3afb09157315039801fdd298a4a0364352fd0
hhs333 <163867431+hhs333@users.noreply.github.com>
2024-12-09 22:02:25 +0300

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,3 +1,78 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "booking_rooms";
+
+try {
+    $dsn = "mysql:host=$servername;dbname=$dbname";
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $userId = $_SESSION['user_id'];
+    $sql = "SELECT * FROM person WHERE users = :user_id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['user_id' => $userId]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($row) {
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $email = $row['Email'];
+        $password = $row['password'];
+
+    } else {
+        echo "User not found!";
+        exit();
+    }
+
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $user_id = htmlspecialchars($_POST['user_id']);
+        $username = htmlspecialchars($_POST['username']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        
+        if (isset($_FILES['profile-pic']) && $_FILES['profile-pic']['error'] === UPLOAD_ERR_OK) {
+            $uploadDir = 'uploads/';
+            $destPath = $uploadDir . basename($_FILES['profile-pic']['name']);
+            
+            if (move_uploaded_file($_FILES['profile-pic']['tmp_name'], $destPath)) {
+                $imageName = $destPath;
+            } else {
+                echo "Error uploading the profile picture.";
+            }
+        }
+
+       
+        $updateSql = "UPDATE users SET Username = :username, Email = :email, Password = :password WHERE User_id = :user_id";
+        $updateStmt = $pdo->prepare($updateSql);
+        $updateData = [
+            'username' => $username,
+            'email' => $email,
+            'password' => $password,
+            'user_id' => $user_id
+        ];
+
+        if ($updateStmt->execute($updateData)) {
+            echo "<p>Profile updated successfully!</p>";
+        } else {
+            echo "<p>Error updating the profile.</p>";
+        }
+    }
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
